adb235dfc81b4cf2507c2cff04d275cbf85413cc
shiyang07ca <shiyang07ca@gmail.comm>
2025-07-21 23:56:03 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+solana_program::entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &solana_program::pubkey::Pubkey,
+    accounts: &[solana_program::account_info::AccountInfo],
+    data: &[u8],
+) -> solana_program::entrypoint::ProgramResult {
+    solana_program::msg!("Hello Solana!");
+    Ok(())
+}
